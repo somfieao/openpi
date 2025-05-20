c45f5be3be41a96cc76a0c0a9e0239f33518b102
ekkanat_tanchavalit <tanchavalit@hotmail.com>
2025-05-20 15:46:47 +0800

    remove os.environ['LEROBOT_HOME'] before import

--- src/openpi/training/data_loader.py
+++ src/openpi/training/data_loader.py
@@ -6,7 +6,6 @@ from typing import Protocol, SupportsIndex, TypeVar
 
 import jax
 import jax.numpy as jnp
-os.environ["LEROBOT_HOME"] = "~/Surgical-pi0/datasets/demo"
 import lerobot.common.datasets.lerobot_dataset as lerobot_dataset
 import numpy as np
 import torch
